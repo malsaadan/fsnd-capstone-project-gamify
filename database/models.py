e432ca74bc7c291350041a21ddcec2387c0eb8d5
malsaadan <mmalsaadan@gmail.com>
2020-08-31 05:22:54 +0300

    Fix config for heroku

--- database/models.py
+++ database/models.py
@@ -5,8 +5,10 @@ import json
 
 database_name = "gamify"
 
-database_path = "postgres://{}@{}/{}".format('mashaelmohammed', 'localhost:5432', database_name)
-
+# local host path
+# database_path = "postgres://{}@{}/{}".format('mashaelmohammed', 'localhost:5432', database_name)
+# heroku path
+database_path = "postgres://vwlmcqzupzkxkj:bf43c8dc10d4b854edb653c98b58df225ebb65e9cdc76902bbe49ee0db9b4101@ec2-3-215-207-12.compute-1.amazonaws.com:5432/dek8claq8cift1"
 db = SQLAlchemy()
 
 # binds a flask application and a SQLAlchemy service
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 AUTH0_CLIENT_ID=5xDwhLNUXk0SmdxZGSWcq81iVaANk3mn
-AUTH0_CALLBACK_URL=https://localhost:5000/callback
+AUTH0_CALLBACK_URL=https://fsnd-gamify.herokuapp.com/callback
 AUTH0_DOMAIN=gamify-fsnd.us.auth0.com
 AUTH0_AUDIENCE=gamify
